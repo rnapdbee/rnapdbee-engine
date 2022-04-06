1f12d900d13e5f661986de8123212ac75c8f34f7
sniochup <snioszekpawel@gmail.com>
2022-04-06 19:35:16 +0200

    feat: engine Spring repository init

---
+++ src/main/java/com/example/rnapdbeeEngine/RnapdbeeEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.rnapdbeeEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnapdbeeEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RnapdbeeEngineApplication.class, args);
+    }
+
+}
