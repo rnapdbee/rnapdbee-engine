c7c61a792e88844d5b7c960a3fd1a8e3407d4879
Zuretto <mikzur4@gmail.com>
2022-09-11 18:18:52 +0200

    ref: make BasePairAnalyzers loadable by loader class

--- src/main/java/pl/poznan/put/rnapdbee/engine/basepair/boundary/Fr3dBasePairAnalyzer.java
+++ src/main/java/pl/poznan/put/rnapdbee/engine/basepair/boundary/Fr3dBasePairAnalyzer.java
@@ -3,13 +3,10 @@ package pl.poznan.put.rnapdbee.engine.basepair.boundary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class Fr3dBasePairAnalyzer extends RNApdbeeAdapterBasePairAnalyzer {
 
     @Autowired
