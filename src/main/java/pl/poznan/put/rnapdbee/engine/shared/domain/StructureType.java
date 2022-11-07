e5dda322f83cfa6689ca9459f779a7e4de4dcfd3
Zuretto <mikzur4@gmail.com>
2022-11-07 18:35:12 +0100

    ref: adjust code to review comments

--- src/main/java/pl/poznan/put/rnapdbee/engine/shared/domain/StructureType.java
+++ src/main/java/pl/poznan/put/rnapdbee/engine/shared/domain/StructureType.java
@@ -4,26 +4,6 @@ package pl.poznan.put.rnapdbee.engine.shared.domain;
  * Enum for Structure Type
  */
 public enum StructureType {
-    STRUCTURE_2D/*("2D")*/,
-    STRUCTURE_3D/*("3D");*/
-
-  /*private final String description;
-
-  StructureType(final String description) {
-    this.description = description;
-  }
-
-  public static StructureType fromDescription(final String description) {
-    for (final StructureType type : StructureType.values()) {
-      if (type.description.equals(description)) {
-        return type;
-      }
-    }
-    throw new IllegalArgumentException("Unknown StructureType: " + description);
-  }
-
-  @Override
-  public String toString() {
-    return description;
-  }*/
+    STRUCTURE_2D,
+    STRUCTURE_3D
 }
