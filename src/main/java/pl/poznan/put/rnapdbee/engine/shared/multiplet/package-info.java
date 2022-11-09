8c8dabaa58127fa6b67f9070413113155139315d
Zuretto <mikzur4@gmail.com>
2022-11-09 20:53:51 +0100

    fix: adjust code to review comments

--- src/main/java/pl/poznan/put/rnapdbee/engine/shared/multiplet/package-info.java
+++ src/main/java/pl/poznan/put/rnapdbee/engine/shared/multiplet/package-info.java
@@ -2,6 +2,6 @@
  * Package contains classes which analyze multiplets (i.e. cases where several nucleotides are
  * interacting with each other)
  * <p>
- * Package has been copied from rnapdbee-engine
+ * Package has been copied from rnapdbee-commons (rnapdbee 2.0)
  */
 package pl.poznan.put.rnapdbee.engine.shared.multiplet;
