552ffd2f1123b97408dcf0e8484497053f0fe59d
Zuretto <mikzur4@gmail.com>
2022-11-18 12:20:47 +0100

    ref: make possible to integration tests to use gurobi MILP implementation
    make converter replacement bean return the same bean as in real environment if the Gurobi environment and license is set up

--- src/test/java/pl/poznan/put/rnapdbee/engine/calculation/testhelp/shared/configuration/TestConverterConfiguration.java
+++ src/test/java/pl/poznan/put/rnapdbee/engine/calculation/testhelp/shared/configuration/TestConverterConfiguration.java
@@ -1,24 +1,63 @@
 package pl.poznan.put.rnapdbee.engine.calculation.testhelp.shared.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import pl.poznan.put.rnapdbee.engine.calculation.testhelp.shared.converter.KnotRemoval;
 import pl.poznan.put.rnapdbee.engine.calculation.testhelp.shared.converter.RNAStructure;
+import pl.poznan.put.rnapdbee.engine.shared.converter.MixedIntegerLinearProgrammingConverter;
 import pl.poznan.put.structure.formats.Converter;
 import pl.poznan.put.structure.formats.ImmutableDefaultDotBracket;
 
+import java.io.IOException;
+
+/**
+ * Class replacing bean of {@link MixedIntegerLinearProgrammingConverter} to bean that implements Dynamic Programming
+ * approach in Pseudoknot removal problem inside Integration Tests.
+ */
 @TestConfiguration
 public class TestConverterConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestConverterConfiguration.class);
+    private static final String GUROBI_WORKING = "Gurobi environment and license is set up on this machine. " +
+            "Using actual MILP implementation for converting bpseq to dbn format in Integration Tests.";
+    private static final String GUROBI_NOT_WORKING = "Gurobi environment or license is not set up on this machine. " +
+            "Using DP_NEW implementation for converting bpseq to dbn format in Integration Tests.";
 
     @Bean
     @Primary
     public Converter converter() {
-        return bpSeq -> {
-            RNAStructure structure = new RNAStructure(bpSeq);
-            structure = KnotRemoval.dynamicProgrammingOneBest(structure);
-            return ImmutableDefaultDotBracket.of(
-                    structure.getSequence(), structure.getDotBracketStructure());
-        };
+        boolean isGurobiUp = checkIfGurobiIsSetUpCorrectly();
+        if (isGurobiUp) {
+            logger.info(GUROBI_WORKING);
+            return new MixedIntegerLinearProgrammingConverter();
+        } else {
+            logger.info(GUROBI_NOT_WORKING);
+            return bpSeq -> {
+                RNAStructure structure = new RNAStructure(bpSeq);
+                structure = KnotRemoval.dynamicProgrammingOneBest(structure);
+                return ImmutableDefaultDotBracket.of(
+                        structure.getSequence(), structure.getDotBracketStructure());
+            };
+        }
+    }
+
+    private boolean checkIfGurobiIsSetUpCorrectly() {
+        Runtime rt = Runtime.getRuntime();
+        Process pr;
+        try {
+            pr = rt.exec("gurobi_cl");
+        } catch (IOException e) {
+            return false;
+        }
+        int statusCode;
+        try {
+            statusCode = pr.waitFor();
+        } catch (InterruptedException e) {
+            return false;
+        }
+        return statusCode == 0;
     }
 }
