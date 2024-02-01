a6934d4bbb3e275e26187199a8fa493da4ea1ad4
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-02-01 15:36:14 +0100

    fix: remove unnecessary parameter

--- deployment.sh
+++ deployment.sh
@@ -4,7 +4,7 @@ image_name="rnapdbee-engine-image"
 container_name="rnapdbee-engine-container"
 rnapdbee_engine_repository_path="."
 
-DOCKER_BUILDKIT=1 docker build --build-arg -t $image_name $rnapdbee_engine_repository_path &&
+DOCKER_BUILDKIT=1 docker build -t $image_name $rnapdbee_engine_repository_path &&
 	docker container rm -f $container_name >/dev/null &&
 	docker create --name $container_name -p 8081:8081 --net rnapdbee-network $image_name &&
 	docker image prune -f >/dev/null &&
